9733d75e55c20a5259f61227b6a6b61716b20549
Stephen Kay <kaystep@umich.edu>
2017-03-29 14:05:38 -0500

    User model with validations

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/home'
 
-  get '/about', to 'static_pages#about'
+  get '/about', to: 'static_pages#about'
 
   get '/signup', to: 'users#new'
 
