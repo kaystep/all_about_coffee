be99d37d10edb64cd78defef16bbc8c39c9d9cfc
Stephen Kay <kaystep@umich.edu>
2017-04-01 11:21:08 -0500

    rough users sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
-  def new
+  def show
+  	@user = User.find(params[:id])
   end
+
+  #Create the user object since the signup page is routed to users new
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		flash[:success] = "Welcome to All About Coffee!"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+  	#only allow this paramaters in and nothing else, also make sure that the user submits them in the signup form
+  	def user_params
+  		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
- 
+  
 
   root 'static_pages#home'
 
   get '/about', to: 'static_pages#about'
 
   get '/signup', to: 'users#new'
+  
+  #used to create all the RESTful actions on the users resource
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
