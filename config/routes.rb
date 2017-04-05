02b0781a026c720f3f7c1cb0f09db447b28312e9
Stephen Kay <kaystep@umich.edu>
2017-04-05 17:59:28 -0500

    Allow for users to follow one another

--- config/routes.rb
+++ config/routes.rb
@@ -21,13 +21,19 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   
   #used to create all the RESTful actions on the users resource
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
   resources :account_activations, only: [:edit]
 
   resources :password_resets,     only: [:new, :create, :edit, :update]
 
   resources :microposts,          only: [:create, :destroy]
+
+  resources :relationships,       only: [:create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
